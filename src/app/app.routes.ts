d5d302a76f503817d0241c6733e29ecb30dd8476
HarriMorales18 <harrinson.moralestejedor@unicolombo.edu.co>
2025-09-19 18:48:03 -0500

    feat: Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { Blog } from './pages/blog/blog';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: "nosotros", component: About},
+  {path: "blog", component: Blog},
+  {path: "contacto", component: Contact}
+  
+
+];
