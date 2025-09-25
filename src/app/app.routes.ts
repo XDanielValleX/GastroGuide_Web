856f4db10026b49e0e16e054161c8ee91bb4f1b9
HarriMorales18 <harrinson.moralestejedor@unicolombo.edu.co>
2025-09-25 16:36:17 -0500

    feat: newViews

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,26 @@
+
 import { Blog } from './pages/blog/blog';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Search } from './pages/search/search';
+import { WatchVideosComponent } from './pages/watch-videos/watch-videos';
+import { Profilecomponent } from './pages/profile/profile';
+
+
+;
 
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: "nosotros", component: About},
   {path: "blog", component: Blog},
-  {path: "contacto", component: Contact}
-  
+  {path: "contacto", component: Contact},
+  {path: "buscar", component: Search},
+  { path: 'watch-videos', component: WatchVideosComponent },
+  {path: "profile", component: Profilecomponent}
+
 
 ];
+
