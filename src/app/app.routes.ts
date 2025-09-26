bc54c6c783f3c11f489be5176233524ee37636e8
HarriMorales18 <harrinson.moralestejedor@unicolombo.edu.co>
2025-09-26 12:18:51 -0500

    feat: views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
-import { Search } from './pages/search/search';
-import { WatchVideosComponent } from './pages/watch-videos/watch-videos';
-import { Profilecomponent } from './pages/profile/profile';
 
 
 ;
@@ -16,10 +13,7 @@ export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: "nosotros", component: About},
   {path: "blog", component: Blog},
-  {path: "contacto", component: Contact},
-  {path: "buscar", component: Search},
-  { path: 'watch-videos', component: WatchVideosComponent },
-  {path: "profile", component: Profilecomponent}
+  {path: "contacto", component: Contact}
 
 
 ];
--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -28,5 +28,4 @@ export class Profilecomponent {
       reader.readAsDataURL(file);
     }
   }
-
 }
