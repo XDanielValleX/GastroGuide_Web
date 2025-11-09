682523ed23a0a09765572c22b034a75fde3b57a1
Harrinson Morales <harrinson.moralestejedor@unicolombo.edu.co>
2025-11-09 00:55:25 -0500

    Feat: Estilos

--- src/app/pages/home2/home2.ts
+++ src/app/pages/home2/home2.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home2',
